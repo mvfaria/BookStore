757c49d3b924cf73260cb65fd0a108e13fac304f
Marcus Vinicius Faria <marcusvinicius.faria@gmail.com>
2021-04-18 19:16:57 +0800

    add order service

---
+++ BookStore.App/OrderService.cs
@@ -0,0 +1,48 @@
+﻿using BookStore.Core.Domain;
+using System.Collections.Generic;
+
+namespace BookStore.App
+{
+    internal class OrderService
+    {
+        internal static Order GetOrder()
+        {
+            return new Order
+            {
+                Books = new List<Book>
+                {
+                    new Book
+                    {
+                        BookName = "Unsolved Crimes",
+                        Category = BookCategory.Crime,
+                        TotalCost = 10.99m
+                    },
+                    new Book
+                    {
+                        BookName = "A Little Love Story",
+                        Category = BookCategory.Romance,
+                        TotalCost = 2.40m
+                    },
+                    new Book
+                    {
+                        BookName = "Heresy",
+                        Category = BookCategory.Fantasy,
+                        TotalCost = 6.80m
+                    },
+                    new Book
+                    {
+                        BookName = "Jack the Ripper",
+                        Category = BookCategory.Crime,
+                        TotalCost = 16.00m
+                    },
+                    new Book
+                    {
+                        BookName = "The Tolkien Years",
+                        Category = BookCategory.Fantasy,
+                        TotalCost = 22.90m
+                    }
+                }
+            };
+        }
+    }
+}
--- BookStore.App/Program.cs
+++ BookStore.App/Program.cs
@@ -11,50 +11,14 @@ namespace BookStore.App
 
         static void Main(string[] args)
         {
-            var order = new Order
-            {
-                Books = new List<Book>
-                {
-                    new Book
-                    {
-                        BookName = "Unsolved Crimes",
-                        Category = BookCategory.Crime,
-                        TotalCost = 10.99m
-                    },
-                    new Book
-                    {
-                        BookName = "A Little Love Story",
-                        Category = BookCategory.Romance,
-                        TotalCost = 2.40m
-                    },
-                    new Book
-                    {
-                        BookName = "Heresy",
-                        Category = BookCategory.Fantasy,
-                        TotalCost = 6.80m
-                    },
-                    new Book
-                    {
-                        BookName = "Jack the Ripper",
-                        Category = BookCategory.Crime,
-                        TotalCost = 16.00m
-                    },
-                    new Book
-                    {
-                        BookName = "The Tolkien Years",
-                        Category = BookCategory.Fantasy,
-                        TotalCost = 22.90m
-                    }
-                }
-            };
-
+            var order = OrderService.GetOrder();
             var calculator = new OrderCalculator(order);
             var crimeDiscount = new CategoryCrimeDiscount();
 
-            Console.WriteLine($"Order Total Cost Before Tax: {calculator.CalculateOrderTotal()}");
-            Console.WriteLine($"Order Total Cost Before Tax With Discount: {calculator.CalculateOrderTotal(discount: crimeDiscount)}");
-            Console.WriteLine($"Order Total Cost After Tax: {calculator.CalculateOrderTotal(GST)}");
-            Console.WriteLine($"Order Total Cost After Tax With Discount: {calculator.CalculateOrderTotal(GST, crimeDiscount)}");
+            Console.WriteLine($"Order Total Cost Before Tax: {calculator.CalculateOrderTotal().ToString("C")}");
+            Console.WriteLine($"Order Total Cost Before Tax With Discount: {calculator.CalculateOrderTotal(discount: crimeDiscount).ToString("C")}");
+            Console.WriteLine($"Order Total Cost After Tax: {calculator.CalculateOrderTotal(GST).ToString("C")}");
+            Console.WriteLine($"Order Total Cost After Tax With Discount: {calculator.CalculateOrderTotal(GST, crimeDiscount).ToString("C")}");
         }
     }
 }
